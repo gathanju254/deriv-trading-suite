2aada8f5209fb7d2874fcdb11e2305c4b4dbd5ff
Dant30 <dankarish.dk@gmail.com>
2026-01-13 19:27:29 +0300

    00080

--- frontend/src/components/Common/index.js
+++ frontend/src/components/Common/index.js
@@ -1,4 +1,4 @@
 // frontend/src/components/Common/index.js
-export { default as Header } from './Header';
-export { default as Sidebar } from './Sidebar';
-export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as Header } from './Header/Header';
+export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as LoadingSpinner } from './LoadingSpinner/LoadingSpinner';
